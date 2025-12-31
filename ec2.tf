c0ec13040bd29ee732dee02d9e9dc0ed5263897b
keroleskhalil2025 <keroleskhalil@gmail.com>
2025-12-31 08:52:00 +0100

    add instance name

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,7 @@
-resource "aws_instance" "myec2" {
+resource "aws_instance" "demo-ec2" {
     ami = "ami-068c0051b15cdb816"
     instance_type = "t3.micro"
+    tags = {
+      Name = "demo-instance"
+    }
 }
